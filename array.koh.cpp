028a216387e7aa4f2bb69af81cc9c26f4ea0a1ec
shan <carroshine153@gmail.com>
2026-04-30 18:23:38 +0800

    Clean up order class and main function structure
    Removed unnecessary comments and cleaned up the order class constructor and methods.

--- array.koh.cpp
+++ array.koh.cpp
@@ -8,26 +8,14 @@ struct Order{
   string orderName;
   int priority;
 
-  public:
-  order (string n, string d, int p){
-    name = n;
-    details = d;
-    priority = p;
-  }
-  # hello po!
-cout << "akin ka na lang <<endl;
-*\ eto  dhiq
-  string getName(){return name};
-  string getDetails(){return details};
-  int getPriority(){return priority};
-  
-  void display(){
-    cout <<name << "-" << details << endl;
-  };
-  #di ko na negegets ang github AHAAHA, parang nadami ang mga branch
-}
+int main() {
+  int choice;
 
-int main(){
+  cout << "\n===== FAST FOOD ORDER SYSTEM =====\n";
+
+
+
+
 
   return 0;
 }
